617fe50ef7f3102ba01b1e8feebc7d319cfffa35
SAURABH KUMAR <33839674+srvkmr130@users.noreply.github.com>
2018-12-29 20:34:10 +0530

    Update ai.py

--- ai.py
+++ ai.py
@@ -32,17 +32,16 @@ class Network(nn.Module):
     
 # Implementing Experience Replay
 
-class ReplayMemory(object):
-    
-    def __init__(self, cap):
-        self.cap = cap
+def __init__(self, capacity):
+        self.capacity = capacity
         self.memory = []
     
     def push(self, event):
         self.memory.append(event)
-        if len(self.memory) > self.cap:
+        if len(self.memory) > self.capacity:
             del self.memory[0]
     
     def sample(self, batch_size):
         samples = zip(*random.sample(self.memory, batch_size))
         return map(lambda x: Variable(torch.cat(x, 0)), samples)
+
