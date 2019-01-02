6dbca928c4f64d4722db2e377209e800b19ed770
SAURABH KUMAR <33839674+srvkmr130@users.noreply.github.com>
2019-01-02 08:05:41 +0530

    Update ai.py

--- ai.py
+++ ai.py
@@ -3,36 +3,36 @@
 # Importing the libraries
 
 import numpy as np
-import os
 import random
+import os
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 import torch.optim as optim
 import torch.autograd as autograd
-import torch.nn as nn
 from torch.autograd import Variable
-import torch.nn.functional as F
-import matplotlib as plt
 
-# The architecture of the Neural Network goes here
+# Creating the architecture of the Neural Network
 
 class Network(nn.Module):
     
-    def __init__(self, input_size, nos_action):#input_size => I/P Layer , nos_action => O/P Layer
+    def __init__(self, input_size, nb_action):
         super(Network, self).__init__()
-        self.nos_action = nos_action
         self.input_size = input_size
-        self.fc1 = nn.Linear(input_size, 35) # Here only one Hidden layer is created with 35 neurons.
-        self.fc2 = nn.Linear(30, nos_action)
+        self.nb_action = nb_action
+        self.fc1 = nn.Linear(input_size, 30) # One Hidden Layer with 30 neurons
+        self.fc2 = nn.Linear(30, nb_action)
     
     def forward(self, state):
         x = F.relu(self.fc1(state))
         q_values = self.fc2(x)
         return q_values
-    
-    
+
 # Implementing Experience Replay
 
-def __init__(self, capacity):
+class ReplayMemory(object):
+    
+    def __init__(self, capacity):
         self.capacity = capacity
         self.memory = []
     
