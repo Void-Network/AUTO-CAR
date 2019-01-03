c69d8a9f0f62692c7b67be85d59209a7d257e1a6
SAURABH KUMAR <33839674+srvkmr130@users.noreply.github.com>
2019-01-03 08:21:17 +0530

    Create map.py

---
+++ map.py
@@ -0,0 +1,17 @@
+# Self Driving Car
+
+# Importing the libraries
+import numpy as np
+from random import random, randint
+import matplotlib.pyplot as plt
+import time
+
+# Importing the Kivy packages
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.graphics import Color, Ellipse, Line
+from kivy.config import Config
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
+from kivy.vector import Vector
+from kivy.clock import Clock
